2961971351833c6d3ff5036feeae2718a170b7e5
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-10-22 11:17:39 +0200

    Initial spring-boot project added

---
+++ fishing-booking-back/src/main/java/com/project/fishingbookingback/FishingBookingApplication.java
@@ -0,0 +1,13 @@
+package com.project.fishingbookingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingBookingApplication.class, args);
+	}
+
+}
---
+++ fishing-booking-back/src/test/java/com/project/fishingbookingback/FishingBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fishingbookingback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
