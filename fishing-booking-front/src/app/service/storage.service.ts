04dcdd61604a0a84f8b51f95fc852fce3193ab5a
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-10-29 14:20:01 +0200

    Added token, user and role storage in local storage

--- fishing-booking-front/src/app/service/storage.service.ts
+++ fishing-booking-front/src/app/service/storage.service.ts
@@ -10,7 +10,9 @@ export class StorageService {
   constructor() { }
   storeTokenData(token: string) : void {
     localStorage.setItem("token", token)
-    let decoded = jwt_decode(token)
+    let decoded: any = jwt_decode(token)
+    localStorage.setItem("user", decoded.sub)
+    localStorage.setItem("role", decoded.role)
     console.log(decoded)
   }
 }
