7f7b51018b0df289dc83feb336eccbca1ebf223a
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-12-15 12:43:02 +0100

    feat: added initial CI pipeline

--- fishing-booking-back/src/test/java/com/project/fishingbookingback/FishingBookingApplicationTests.java
+++ fishing-booking-back/src/test/java/com/project/fishingbookingback/FishingBookingApplicationTests.java
@@ -2,12 +2,33 @@ package com.project.fishingbookingback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class FishingBookingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final WebApplicationContext webApplicationContext;
+
+    public FishingBookingApplicationTests(WebApplicationContext webApplicationContext) {
+        this.webApplicationContext = webApplicationContext;
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    public void verifyServletContext() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+    }
 
 }
