640b1ac87f090a02d9b0cac52ce0dab40f61defb
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-12-27 18:38:26 +0100

    feat: reservation view changed

--- fishing-booking-front/src/app/material/material.module.ts
+++ fishing-booking-front/src/app/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 const MaterialCompontents:any = [
   MatButtonModule,
   MatIconModule,
-  MatProgressSpinnerModule
+  MatProgressSpinnerModule,
+  MatGridListModule
 ];
 
 @NgModule({
--- fishing-booking-front/src/app/service/reservation.service.ts
+++ fishing-booking-front/src/app/service/reservation.service.ts
@@ -26,6 +26,40 @@ export class ReservationService {
       durationInHours: 3,
       mark: 5.3,
       adress: "Alley 12"
+      },
+      {imgUrl: "",
+      name: "Adventure",
+      startDate: new Date(2017, 0O5, 0O5, 17, 23, 42, 11),
+      price: 23,
+      durationInHours: 4,
+      mark: 2.2,
+      adress: "Cloverfiel 22"
+    },
+      {
+      imgUrl: "",
+      name: "Adventure2",
+      startDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11),
+      price: 24,
+      durationInHours: 3,
+      mark: 5.3,
+      adress: "Alley 12"
+      },
+      {imgUrl: "",
+      name: "Adventure",
+      startDate: new Date(2017, 0O5, 0O5, 17, 23, 42, 11),
+      price: 23,
+      durationInHours: 4,
+      mark: 2.2,
+      adress: "Cloverfiel 22"
+    },
+      {
+      imgUrl: "",
+      name: "Adventure2",
+      startDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11),
+      price: 24,
+      durationInHours: 3,
+      mark: 5.3,
+      adress: "Alley 12"
       }];
   }
 }
