cba065769134f8dc298b28df6321c3261f73edbf
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-01-13 01:52:14 +0100

    feat: created fronted for reserving adventures

--- fishing-booking-front/src/app/component/client-homepage/reserve-adventure/reserve-adventure.component.ts
+++ fishing-booking-front/src/app/component/client-homepage/reserve-adventure/reserve-adventure.component.ts
@@ -10,11 +10,40 @@ export class ReserveAdventureComponent implements OnInit {
   constructor() { }
 
   @Input() adventure: any
+  totalPrice: number = 0;
+  chosenDate: Date= new Date();
+  startingDate: string = "";
+  endingDate: string = ""; 
+  personNum: number = 1;
 
   ngOnInit(): void {
   }
 
+  convertTimeToNum(time: string): number{
+    let timeNum = 0;
+    let minutes = parseInt(time.split(" ")[0].split(":")[1]);
+    let hours = parseInt(time.split(" ")[0].split(":")[0]);
+    if(time.split(" ")[1]=="PM")
+      timeNum +=12 * 60;
+    timeNum+=+ hours*60 + minutes;
+  
+    return timeNum;
+  }
+
+
   reserve(): void{
-    
+    if(this.totalPrice<=0){alert("Please calculate price first!"); return;}
+  }
+
+  periodDurationInMinutes(): number{
+    return this.convertTimeToNum(this.endingDate)- this.convertTimeToNum(this.startingDate);
+  }
+
+  calculatePrice(): void{
+    let price = this.periodDurationInMinutes()/60 * this.personNum * this.adventure.hourlyPrice;
+    if(price < 0){
+      alert("Please choose valid times!"); return;
+    }
+    this.totalPrice = this.periodDurationInMinutes()/60 * this.personNum * this.adventure.hourlyPrice;
   }
 }
